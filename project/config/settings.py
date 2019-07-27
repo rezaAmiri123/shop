2ba1cb065c9125cc8d509fca16a34556858f16f8
rezaAmiri123 <reza.amiri.ac@gmail.com>
2019-07-27 11:46:01 +0430

    circleci has been added

--- project/config/settings.py
+++ project/config/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static1'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static1'),
+                    os.path.join(BASE_DIR, 'static'))
 
 from django.utils.translation import gettext_lazy as _
 
