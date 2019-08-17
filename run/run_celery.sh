141872111922efa814ea9370ca2e84452615f102
rezaAmiri123 <reza.amiri.ac@gmail.com>
2019-08-17 22:55:57 +0430

    fixing the docker compose

--- run/run_celery.sh
+++ run/run_celery.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # wait for PSQL server to start
-# sleep 10
+sleep 10
 
 cd project
-celery worker -A config -l info
+#celery worker -A config -l info
+celery worker -A config -Q default -n default@%h
